ed7d052c1e605fc308c3a968049ef30b016179ed
Ryan Wilkinson <ryyguy9@gmail.com>
2023-04-27 15:08:50 -0400

    Time complexities done, paper now.

--- main.py
+++ main.py
@@ -98,31 +98,32 @@ def delivery(truck, listAddresses, list2DDistances, hTable):  #
 
 
 def all_delivery_for_time_input1():  # function that gets all the delivery times of delivery truck one to be used for a user input
-    deliveries_t1 = delivery(truckone, address_list, distance_list, myhash)
+    deliveries_t1 = delivery(truckone, address_list, distance_list, myhash)  #  O(n^2 log n)
     delivered_packages = deliveries_t1[3]
     return delivered_packages
 
 
 def all_delivery_for_time_input2():  # # function that gets all the delivery times of delivery truck two to be used for a user input
-    deliveries_t1 = delivery(trucktwo, address_list, distance_list, myhash)
+    deliveries_t1 = delivery(trucktwo, address_list, distance_list, myhash)  #  O(n^2 log n)
     t2 = deliveries_t1[3]
     return t2
 
 
 def all_delivery_for_time_input3():  # # function that gets all the delivery times of delivery truck three to be used for a user input
-    deliveries_t3 = delivery(truckthree, address_list, distance_list, myhash)
+    deliveries_t3 = delivery(truckthree, address_list, distance_list, myhash)  # O(n^2 log n)
     t3 = deliveries_t3[3]
     return t3
 
 
+#  total time complexity is O(n^2 log n) * O(n^2 log n) * O(n^2 log n) = O(n^6 log^3 n)
 def total_input():  # function which calls all the other time input delivery functions to combine them
-    all_delivery_for_time_input1()
-    all_delivery_for_time_input2()
-    all_delivery_for_time_input3()
+    all_delivery_for_time_input1()  # O (n^2 log n)
+    all_delivery_for_time_input2()  # O(n^2 log n)
+    all_delivery_for_time_input3()  # O(n^2 log n)
 
 
 # Function that will give status of all packages based on a given time
-def package_time_status(given_time, h_table, packages):  # time complexity: O(n)
+def package_time_status(given_time, h_table, packages):  # time complexity: O(n^6 log^3 n) * n = O(N^6 log^3 N)
     total_input()  # calls the total input function
     for i in range(1, packages + 1):  # loops through packages in hash table
             pck = h_table.search(i)  # gets information with search method
@@ -136,7 +137,7 @@ def package_time_status(given_time, h_table, packages):  # time complexity: O(n)
 
 
 # function that loops through the delivery information of each package id and tells when it was delivered
-def all_delivery_t1():  #  Time complexity: O(N)
+def all_delivery_t1():  #  Time complexity: O(n log n)
     deliveries_t1 = delivery(truckone, address_list, distance_list, myhash)
     delivered_packages = deliveries_t1[3]
     print("Delivery times for truck one:")
@@ -146,7 +147,7 @@ def all_delivery_t1():  #  Time complexity: O(N)
 
 
 # function that loops through the delivery information of each package id and tells when it was delivered
-def all_delivery_t2():  # Time complexity: O(N)
+def all_delivery_t2():  # Time complexity: O(n log n)
     deliveries_t2 = delivery(trucktwo, address_list, distance_list, myhash)
     delivered_packages = deliveries_t2[3]
     print("Delivery times for truck 2:")
@@ -156,7 +157,7 @@ def all_delivery_t2():  # Time complexity: O(N)
 
 
 # function that loops through the delivery information of each package id and tells when it was delivered
-def all_delivery_t3():  # Time complexity: O(N)
+def all_delivery_t3():  # Time complexity: O(n log n)
     deliveries_t3 = delivery(truckthree, address_list, distance_list, myhash)
     delivered_packages = deliveries_t3[3]
     print("Delivery times for truck 3:")
@@ -166,13 +167,14 @@ def all_delivery_t3():  # Time complexity: O(N)
 
 
 # Function which calls the three all_delivery functions to get the times of all functions
-def all_deliveries():  # Time complexity is O(n) * O(n) * O(n) = O(n^3)
+def all_deliveries():  # Time complexity is O(n log n) * O(n log n) * O(n log n) = O(n^6 log^3 n )
     all_delivery_t1()
     all_delivery_t2()
     all_delivery_t3()
 
 
 # function which returns the delivery time of a single package given its ID
+# time complexity is O(n log n) * O(n log n) * O(n log n) = O(n^6 log^3 n)
 def delivery_by_id():
     id = input("Enter ID: ")  # input for user to enter an ID
     d1 = delivery(truckone, address_list, distance_list, myhash)  # call the delivery function on all 3 trucks
