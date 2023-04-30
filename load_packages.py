ef65a2647f48478235486517116a386839f0f04d
Ryan Wilkinson <ryyguy9@gmail.com>
2023-04-30 11:21:44 -0400

    Remade package list for trucks due to 25 and 34 not being delivered on time.

--- load_packages.py
+++ load_packages.py
@@ -10,21 +10,21 @@ from Package import Package
 from csv_reader import load_package_data
 from csv_reader import myhash
 from delivery_truck import DeliveryTruck
-
+# 1 6 13 14 15 16 20 25 29 30 31 34 37 40
 # create array to hold package id's for each individual truck
 # assorted packages according to requirements
 
-t1 = [2, 21, 10, 22, 17, 23, 40, 24, 39, 26, 35, 29, 30, 27]
-t2 = [8, 3, 4, 36, 5, 38, 12, 18, 11, 16, 20, 15, 13, 14, 19]
-t3 = [37, 1, 34, 9, 33, 25, 7, 28, 6, 31, 32]
+t1 = [27, 35, 13, 14, 15, 16, 19, 20, 21, 39, 4, 40, 7, 29, 11, 22]
+t2 = [3,36,18, 5, 37, 38, 10, 12, 17, 8, 30, 2, 33, 6, 31]
+t3 = [32, 28, 9, 23, 24, 26, 1,34,25]
 TODAY = datetime.now()
 
 print(TODAY)  # prints today date and time when the program is run
 # Start time for each of the delivery trucks
 START = datetime(TODAY.year, TODAY.month, TODAY.day, 8, 0, 0, 0)  # start time for first truck
-START_two = datetime(TODAY.year, TODAY.month, TODAY.day, 9, 0, 0, 0)  # start time for second truck
-START_three = datetime(TODAY.year, TODAY.month, TODAY.day, 10, 30, 0, 0)  # start time for third truck
-truckone = delivery_truck.DeliveryTruck(16, .3, t1, "4001 South 700 East", 0.0, START, START)  #initializing truck object
+START_two = datetime(TODAY.year, TODAY.month, TODAY.day, 8, 0, 0, 0)  # start time for second truck
+START_three = datetime(TODAY.year, TODAY.month, TODAY.day, 9, 45, 0, 0)  # start time for third truck
+truckone = delivery_truck.DeliveryTruck(16, .3, t1, "4001 South 700 East", 0.0, START, START)  # initializing truck object
 for i in t1:
     myhash.search(i).load_time = START
     myhash.search(i).package_delivery_status = "TRANSIT"
